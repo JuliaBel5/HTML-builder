a8c2175c8a077cbe4fa9a7b763b16da18c385f40
JuliaBel5 <juliabel5@gmail.com>
2024-01-22 18:22:39 +0300

    refactor: refactor 6th task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -6,39 +6,28 @@ const target = path.join(__dirname, 'project-dist');
 const source = path.join(__dirname, 'styles');
 const stylesFile = path.join(`${__dirname}/project-dist`, 'style.css');
 
-async function delFolder(target) {
-  const files = await fsp.readdir(target); // чтение содержимого
-  for await (const file of files) {
-    const filePath = path.join(target, file); //  полный путь к файлу/папке
-    const item = await fsp.stat(filePath); // инфо о файле/папке
-
-    if (item.isDirectory()) {
-      await delFolder(filePath);
-    } else {
-      // если это файл
-      await fsp.unlink(filePath);
-    }
-  }
-}
-
 async function createHtml() {
   try {
-    await delFolder(target);
-  } catch (error) {
-    if (error.code !== 'ENOENT') throw error;
-  }
+    await fsp.rm(target, {
+      recursive: true,
+      force: true,
+      maxRetries: 1000,
+    });
 
-  try {
     await fsp.mkdir(path.resolve(__dirname, 'project-dist'));
   } catch (error) {
-    if (error.code !== 'EEXIST') throw error;
+    console.log(error);
   }
+
   createBundle();
+
   let template = await fsp.readFile(
     path.resolve(__dirname, 'template.html'),
     'utf8',
   );
+
   const htmlItems = await fsp.readdir(path.resolve(__dirname, 'components'));
+
   for (const htmlItem of htmlItems) {
     if (path.extname(htmlItem) === '.html') {
       console.log(htmlItem);
@@ -50,18 +39,27 @@ async function createHtml() {
       template = template.replace(`{{${fileName}}}`, htmlContent);
     }
   }
+
   await fsp.writeFile(
     path.resolve(path.resolve(__dirname, 'project-dist'), 'index.html'),
     template,
   );
   console.log('Файл index.html успешно создан');
-  await copyFiles(
+
+  fs.cp(
     path.join(__dirname, 'assets'),
     path.join(__dirname, 'project-dist', 'assets'),
+    { recursive: true },
+    (err) => {
+      if (err) return;
+
+      console.log('Папка assets успешно скопирована');
+    },
   );
-  console.log('Папка assets успешно скопирована');
 }
+
 createHtml();
+
 function createBundle() {
   fs.readdir(source, (err, files) => {
     if (err) {
@@ -94,22 +92,4 @@ function createBundle() {
     }
   });
   console.log('Файл style.css успешно сформирован');
-}
-
-async function copyFiles(source, target) {
-  await fsp.mkdir(target, { recursive: true });
-
-  const files = await fsp.readdir(source, { withFileTypes: true });
-  for (const file of files) {
-    if (file.isFile()) {
-      await fsp.copyFile(
-        path.join(source, file['name']),
-        path.join(target, file['name']),
-      );
-    } else {
-      let newSource = path.join(source, file.name);
-      let newTarget = path.join(target, file.name);
-      await copyFiles(newSource, newTarget);
-    }
-  }
 }
